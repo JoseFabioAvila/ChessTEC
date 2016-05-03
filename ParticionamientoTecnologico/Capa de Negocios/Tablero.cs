353b153a880a78ad00e1392466aecf99c27ecc79
Jose Fabio Avila Estrada <fabioavila-16@hotmail.com>
2016-05-02 22:11:02 -0600

    avance 2

--- ParticionamientoTecnologico/Capa de Negocios/Tablero.cs
+++ ParticionamientoTecnologico/Capa de Negocios/Tablero.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChessTEC.ParticionamientoTecnologico.Capa_de_Negocios.Piezas;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,62 @@ namespace ChessTEC.ParticionamientoTecnologico.Capa_de_Negocios
 {
     class Tablero
     {
+        public Pieza[][] matrizTablero;
+
+        public Tablero() {
+            matrizTablero = new Pieza[8][];
+            for (int i = 0; i < matrizTablero.Length; i++)
+            {
+                //Console.WriteLine(i.ToString());
+                
+                matrizTablero[i] = new Pieza[8];
+
+            }
+            colocarFichas();
+        }
+
+        private void colocarFichas()
+        {
+            
+            //fichas player 1
+            matrizTablero[0][0] = new Torre("player2");
+            matrizTablero[0][1] = new Caballo("player2");
+            matrizTablero[0][2] = new Alfil("player2");
+            matrizTablero[0][3] = new Rey("player2");
+            matrizTablero[0][4] = new Reina("player2");
+            matrizTablero[0][5] = new Alfil("player2");
+            matrizTablero[0][6] = new Caballo("player2");
+            matrizTablero[0][7] = new Torre("player2");
+
+            matrizTablero[1][0] = new Peon("player2");
+            matrizTablero[1][1] = new Peon("player2");
+            matrizTablero[1][2] = new Peon("player2");
+            matrizTablero[1][3] = new Peon("player2");
+            matrizTablero[1][4] = new Peon("player2");
+            matrizTablero[1][5] = new Peon("player2");
+            matrizTablero[1][6] = new Peon("player2");
+            matrizTablero[1][7] = new Peon("player2");
+
+
+            //fichas player 2
+            matrizTablero[6][0] = new Peon("player1");
+            matrizTablero[6][1] = new Peon("player1");
+            matrizTablero[6][2] = new Peon("player1");
+            matrizTablero[6][3] = new Peon("player1");
+            matrizTablero[6][4] = new Peon("player1");
+            matrizTablero[6][5] = new Peon("player1");
+            matrizTablero[6][6] = new Peon("player1");
+            matrizTablero[6][7] = new Peon("player1");
+
+            matrizTablero[7][0] = new Torre("player1");
+            matrizTablero[7][1] = new Caballo("player1");
+            matrizTablero[7][2] = new Alfil("player1");
+            matrizTablero[7][3] = new Rey("player1");
+            matrizTablero[7][4] = new Reina("player1");
+            matrizTablero[7][5] = new Alfil("player1");
+            matrizTablero[7][6] = new Caballo("player1");
+            matrizTablero[7][7] = new Torre("player1");
+        }
     }
+
 }
--- ParticionamientoTecnologico/Capa de Presentacion/Form1.cs
+++ ParticionamientoTecnologico/Capa de Presentacion/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChessTEC.ParticionamientoTecnologico.Capa_de_Negocios;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,112 @@ namespace ChessTEC
 {
     public partial class Form1 : Form
     {
+        PictureBox[][] matrizBotones;
+        Tablero tablero;
+        
         public Form1()
         {
             InitializeComponent();
+            tablero = new Tablero();
+            cargarBotones();
+            updateTablero();
+        }
+
+        public void cargarBotones()
+        {
+            matrizBotones = new PictureBox[8][];
+            for (int i = 0; i < matrizBotones.Length; i++)
+            {
+                matrizBotones[i] = new PictureBox[8];
+            }
+
+            matrizBotones[0][0] = p1A;
+            matrizBotones[0][1] = p1B;
+            matrizBotones[0][2] = p1C;
+            matrizBotones[0][3] = p1D;
+            matrizBotones[0][4] = p1E;
+            matrizBotones[0][5] = p1F;
+            matrizBotones[0][6] = p1G;
+            matrizBotones[0][7] = p1H;
+
+            matrizBotones[1][0] = p2A;
+            matrizBotones[1][1] = p2B;
+            matrizBotones[1][2] = p2C;
+            matrizBotones[1][3] = p2D;
+            matrizBotones[1][4] = p2E;
+            matrizBotones[1][5] = p2F;
+            matrizBotones[1][6] = p2G;
+            matrizBotones[1][7] = p2H;
+
+            matrizBotones[2][0] = p3A;
+            matrizBotones[2][1] = p3B;
+            matrizBotones[2][2] = p3C;
+            matrizBotones[2][3] = p3D;
+            matrizBotones[2][4] = p3E;
+            matrizBotones[2][5] = p3F;
+            matrizBotones[2][6] = p3G;
+            matrizBotones[2][7] = p3H;
+
+            matrizBotones[3][0] = p4A;
+            matrizBotones[3][1] = p4B;
+            matrizBotones[3][2] = p4C;
+            matrizBotones[3][3] = p4D;
+            matrizBotones[3][4] = p4E;
+            matrizBotones[3][5] = p4F;
+            matrizBotones[3][6] = p4G;
+            matrizBotones[3][7] = p4H;
+            
+            matrizBotones[4][0] = p5A;
+            matrizBotones[4][1] = p5B;
+            matrizBotones[4][2] = p5C;
+            matrizBotones[4][3] = p5D;
+            matrizBotones[4][4] = p5E;
+            matrizBotones[4][5] = p5F;
+            matrizBotones[4][6] = p5G;
+            matrizBotones[4][7] = p5H;
+
+            matrizBotones[5][0] = p6A;
+            matrizBotones[5][1] = p6B;
+            matrizBotones[5][2] = p6C;
+            matrizBotones[5][3] = p6D;
+            matrizBotones[5][4] = p6E;
+            matrizBotones[5][5] = p6F;
+            matrizBotones[5][6] = p6G;
+            matrizBotones[5][7] = p6H;
+
+            matrizBotones[6][0] = p7A;
+            matrizBotones[6][1] = p7B;
+            matrizBotones[6][2] = p7C;
+            matrizBotones[6][3] = p7D;
+            matrizBotones[6][4] = p7E;
+            matrizBotones[6][5] = p7F;
+            matrizBotones[6][6] = p7G;
+            matrizBotones[6][7] = p7H;
+
+            matrizBotones[7][0] = p8A;
+            matrizBotones[7][1] = p8B;
+            matrizBotones[7][2] = p8C;
+            matrizBotones[7][3] = p8D;
+            matrizBotones[7][4] = p8E;
+            matrizBotones[7][5] = p8F;
+            matrizBotones[7][6] = p8G;
+            matrizBotones[7][7] = p8H;            
+        }
+
+        private void updateTablero()
+        {
+            for (int i = 0; i < tablero.matrizTablero.Length; i++)
+            {
+                for (int j = 0; j < tablero.matrizTablero[i].Length; j++)
+                {
+                    if (tablero.matrizTablero[i][j] != null)
+                    {
+                        //Console.WriteLine(tablero.matrizTablero[i][j].imagen);
+                        matrizBotones[i][j].ImageLocation = tablero.matrizTablero[i][j].imagen;
+
+                    }
+                }
+            }
         }
 
         private void p1A_Click(object sender, EventArgs e)
